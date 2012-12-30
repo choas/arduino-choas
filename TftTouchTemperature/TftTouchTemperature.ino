241d2d46a60721672b5b4de936678ec7e262f78d
Lars Gregori <lars.gregori@gmail.com>
2012-12-30 13:23:24 +0100

    added TFT Touch Shield copyright notice

--- TftTouchTemperature/TftTouchTemperature.ino
+++ TftTouchTemperature/TftTouchTemperature.ino
@@ -6,6 +6,46 @@
 #include <TFT.h>
 
 
+
+// Copyright notice for TFT Touch Shield:
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifdef SEEEDUINO
+  #define YP A2   // must be an analog pin, use "An" notation!
+  #define XM A1   // must be an analog pin, use "An" notation!
+  #define YM 14   // can be a digital pin, this is A0
+  #define XP 17   // can be a digital pin, this is A3 
+#endif
+
+#ifdef MEGA
+  #define YP A2   // must be an analog pin, use "An" notation!
+  #define XM A1   // must be an analog pin, use "An" notation!
+  #define YM 54   // can be a digital pin, this is A0
+  #define XP 57   // can be a digital pin, this is A3 
+#endif 
+
+
+#define TS_MINX 140
+#define TS_MAXX 900
+
+#define TS_MINY 120
+#define TS_MAXY 940
+
+
+
+
 // OneWire DS18S20, DS18B20, DS1822 Temperature Example
 //
 // http://www.pjrc.com/teensy/td_libs_OneWire.html
@@ -16,6 +56,17 @@
 
 OneWire  ds(A4);  // on pin A4
 
+
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// The 2.8" TFT Touch shield has 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+
+
+
+
 void setup(void) {
   Serial.begin(9600);
   
@@ -23,8 +74,34 @@ void setup(void) {
   initDraw();
 }
 
-
 void loop(void) {
+  
+    // a point object holds x y and z coordinates
+  Point p = ts.getPoint();
+
+  if (p.z > ts.pressureThreshhold) {
+     Serial.print("Raw X = "); Serial.print(p.x);
+     Serial.print("\tRaw Y = "); Serial.print(p.y);
+     Serial.print("\tPressure = "); Serial.println(p.z);
+  }
+  
+ 
+  p.x = map(p.x, TS_MINX, TS_MAXX, 240, 0);
+  p.y = map(p.y, TS_MINY, TS_MAXY, 320, 0);
+  
+  // we have some minimum pressure we consider 'valid'
+  // pressure of 0 means no pressing!
+  if (p.z > ts.pressureThreshhold) {
+     Serial.print("X = "); Serial.print(p.x);
+     Serial.print("\tY = "); Serial.print(p.y);
+     Serial.print("\tPressure = "); Serial.println(p.z);
+  }
+
+  
+  loopTemperature();
+}  
+
+void loopTemperature() {
   byte i;
   byte present = 0;
   byte type_s;
@@ -120,14 +197,6 @@ void loop(void) {
 
   drawTemp(celsius, addr[7] == 0x6B ? 1 : 0);
 }
-
-
-// For better pressure precision, we need to know the resistance
-// between X+ and X- Use any multimeter to read it
-// The 2.8" TFT Touch shield has 300 ohms across the X plate
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
-
-
 
 void initDraw() {
   Tft.drawString("Temperature", 20, 25, 2, WHITE);
